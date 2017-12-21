49e27c3e37c4d03060e9fa2db77d08385318a729
Unknown <ecr.referee@hotmail.com>
2017-12-20 18:08:44 -0600

    Basic filter

--- SlackerBot/CommandHandler.cs
+++ SlackerBot/CommandHandler.cs
@@ -21,38 +21,40 @@ namespace SlackerBot
 
         private async Task _client_MessageReceived(SocketMessage arg) {
             if (arg is SocketUserMessage msg) {
-                var context = new SocketCommandContext(_client, msg);
+                if (msg.Channel.Name.Contains("bot-testing-room")) {
+                    var context = new SocketCommandContext(_client, msg);
 
-                var argPos = 0;
-                if (msg.HasCharPrefix('!', ref argPos)) {
-                    var result = await _service.ExecuteAsync(context, argPos);
-                    if (!result.IsSuccess && result.Error != CommandError.UnknownCommand) {
-                        await context.Channel.SendMessageAsync(result.ErrorReason);
+                    var argPos = 0;
+                    if (msg.HasCharPrefix('!', ref argPos)) {
+                        var result = await _service.ExecuteAsync(context, argPos);
+                        if (!result.IsSuccess && result.Error != CommandError.UnknownCommand) {
+                            await context.Channel.SendMessageAsync(result.ErrorReason);
+                        }
+                    } else if (msg.Content.Contains("Jake")
+                        || msg.Content.Contains("jake")
+                        || msg.Content.Contains("Jacob")
+                        || msg.Content.Contains("jacob")) {
+                        await context.Channel.SendMessageAsync("It's \"Jakcbo\", friend");
+                    } else if (msg.Content.Contains("Haley")
+                         || msg.Content.Contains("haley")) {
+                        await context.Channel.SendMessageAsync("It's \"Helee\", friend");
+                    } else if (msg.Content.Contains("Kansas")
+                         || msg.Content.Contains("kansas")
+                         || msg.Content.Contains("Joe")
+                         || msg.Content.Contains("joe")) {
+                        await context.Channel.SendMessageAsync("It's \"Mr. Angry\", friend");
+                    } else if (msg.Content.Contains("Chris")
+                         || msg.Content.Contains("chris")) {
+                        await context.Channel.SendMessageAsync("It's \"Chrus\", friend");
+                    } else if (msg.Content.Contains("Izaak")
+                          || msg.Content.Contains("izaak")) {
+                        await context.Channel.SendMessageAsync("It's \"The Greg\", friend");
+                    } else if (msg.Content.Contains("Stephanie")
+                          || msg.Content.Contains("stephanie")
+                          || msg.Content.Contains("Steph")
+                          || msg.Content.Contains("steph")) {
+                        await context.Channel.SendMessageAsync("It's \"freshman\", friend");
                     }
-                } else if (msg.Content.Contains("Jake")
-                    || msg.Content.Contains("jake")
-                    || msg.Content.Contains("Jacob")
-                    || msg.Content.Contains("jacob")) {
-                    await context.Channel.SendMessageAsync("It's \"Jakcbo\", friend");
-                } else if (msg.Content.Contains("Haley")
-                     || msg.Content.Contains("haley")) {
-                    await context.Channel.SendMessageAsync("It's \"Helee\", friend");
-                } else if (msg.Content.Contains("Kansas")
-                     || msg.Content.Contains("kansas")
-                     || msg.Content.Contains("Joe")
-                     || msg.Content.Contains("joe")) {
-                    await context.Channel.SendMessageAsync("It's \"Mr. Angry\", friend");
-                } else if (msg.Content.Contains("Chris")
-                     || msg.Content.Contains("chris")) {
-                    await context.Channel.SendMessageAsync("It's \"Chrus\", friend");
-                } else if (msg.Content.Contains("Izaak")
-                      || msg.Content.Contains("izaak")) {
-                    await context.Channel.SendMessageAsync("It's \"The Greg\", friend");
-                } else if (msg.Content.Contains("Stephanie")
-                      || msg.Content.Contains("stephanie")
-                      || msg.Content.Contains("Steph")
-                      || msg.Content.Contains("steph")) {
-                    await context.Channel.SendMessageAsync("It's \"freshman\", friend");
                 }
             }
         }
