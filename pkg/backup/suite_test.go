097d96ec7469c9e4b57bc85a3e21145623d7e895
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-03-21 11:41:42 -0500

    Test logging

--- pkg/backup/suite_test.go
+++ pkg/backup/suite_test.go
@@ -1,13 +1,16 @@
 package backup
 
 import (
+	"io"
 	"testing"
 
+	"charm.land/log/v2"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
 func TestBackup(t *testing.T) {
+	log.SetDefault(log.New(io.Discard))
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Backup Suite")
 }
